b190b64a4f559c6b1a465d333f7379941940daa6
gjmelief <gjmelief@gmail.com>
2026-02-12 20:37:05 +0100

    Add calculate_average function
    - Implement average calculation using sum and len
    - Use list() to convert dict values for calculation
    - Round result to 1 decimal place
    - Add __main__ block for development testing

--- statistics.py
+++ statistics.py
@@ -9,6 +9,14 @@ from data_handler import read_log
 
 def calculate_average(sensor_data, sensor_name):
     """Calculate average for specific sensor.
-    sensor_data = nested dict (output van read_log)
+    sensor_data = nested dict (output of read_log)
     sensor_name = e.g. "PI-001"
     """
+
+    readings = list(sensor_data[sensor_name].values())
+    average = round((sum(readings) / len(readings)), 1)
+    return average
+
+if __name__ == "__main__":
+    sensor_data = read_log()
+    print(calculate_average(sensor_data, "PI-001"))
